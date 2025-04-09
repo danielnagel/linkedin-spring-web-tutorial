1c4f2c491984d3997afb6ce9c370f31401ee5c9c
Daniel Nagel <danielnagel@mailbox.org>
2025-04-09 09:32:28 +0200

    Add GreetingWebController and greeting.html template

---
+++ src/main/java/linkedin/web/GreetingWebController.java
@@ -0,0 +1,17 @@
+package linkedin.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/www/greeting")
+public class GreetingWebController {
+    
+    @GetMapping
+    public String index(Model model) {
+        model.addAttribute("message", "Hello, World!");
+        return "greeting";
+    }
+}
